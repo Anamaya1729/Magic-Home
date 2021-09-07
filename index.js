fcacc790aab8e4280918ff1fd76d73a80bf7fdf1
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-09-07 22:05:32 +0530

    New

---
+++ index.js
@@ -0,0 +1,8 @@
+const searchItem = document.getElementById("search-el")
+const searchButton = document.getElementById("seachButton-el")
+const searchResult = document.getElementById("searchQuery")
+
+
+function search(){
+    console.log(searchItem.value)
+}
